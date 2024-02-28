6c61ad113695cd898da4c24bfdfcf3c9c015a5b0
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 16:34:47 +0000

    Completing Exercice 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insire número inteiro não-negativo: ");
+            byte n = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"{--n}");
+            Console.WriteLine($"{++n}");
+            
+        }
+    }
+}
