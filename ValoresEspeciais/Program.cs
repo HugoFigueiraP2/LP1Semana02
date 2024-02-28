823bce41d03ff9422993a785b0471db2da9aed03
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 17:32:24 +0000

    Completed Exercise 6

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -10,8 +10,13 @@ namespace ValoresEspeciais
             Console.WriteLine($"Tipo short - (Minimo): {short.MinValue} || (Máximo) : {short.MaxValue}");
             Console.WriteLine($"Tipo long - (Minimo): {long.MinValue} || (Máximo) : {long.MaxValue}");
             Console.WriteLine($"Tipo float - (Minimo): {float.MinValue} || (Máximo) : {float.MaxValue}");
+            Console.WriteLine($"Tipo float - (Infinito Positivo): {float.PositiveInfinity} || (Infinito Negativo) : {float.NegativeInfinity}");
+            Console.WriteLine($"Tipo float - (NaN): {float.NaN}");
             Console.WriteLine($"Tipo double - (Minimo): {double.MinValue} || (Máximo) : {double.MaxValue}");
+            Console.WriteLine($"Tipo double - (Infinito Positivo): {double.PositiveInfinity} || (Infinito Negativo) : {double.NegativeInfinity}");
+            Console.WriteLine($"Tipo double - (NaN): {double.NaN}");
             Console.WriteLine($"Tipo decimal - (Minimo): {decimal.MinValue} || (Máximo) : {decimal.MaxValue}");
+            
         }
     }
 }
