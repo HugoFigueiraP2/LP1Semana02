ddf807fae68dc21ef71a50cad85dece8e064637b
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 17:24:03 +0000

    Adding Max and Min values for diferent types

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Tipo int - (Minimo): {int.MinValue} || (Máximo) : {int.MaxValue}");
+            Console.WriteLine($"Tipo short - (Minimo): {short.MinValue} || (Máximo) : {short.MaxValue}");
+            Console.WriteLine($"Tipo long - (Minimo): {long.MinValue} || (Máximo) : {long.MaxValue}");
+            Console.WriteLine($"Tipo float - (Minimo): {float.MinValue} || (Máximo) : {float.MaxValue}");
+            Console.WriteLine($"Tipo double - (Minimo): {double.MinValue} || (Máximo) : {double.MaxValue}");
+            Console.WriteLine($"Tipo decimal - (Minimo): {decimal.MinValue} || (Máximo) : {decimal.MaxValue}");
+        }
+    }
+}
