0b03364a326ee0bd3981dadbafbe6baabca48d44
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 17:39:55 +0000

    uint type overflow

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,16 +6,9 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Tipo int - (Minimo): {int.MinValue} || (Máximo) : {int.MaxValue}");
-            Console.WriteLine($"Tipo short - (Minimo): {short.MinValue} || (Máximo) : {short.MaxValue}");
-            Console.WriteLine($"Tipo long - (Minimo): {long.MinValue} || (Máximo) : {long.MaxValue}");
-            Console.WriteLine($"Tipo float - (Minimo): {float.MinValue} || (Máximo) : {float.MaxValue}");
-            Console.WriteLine($"Tipo float - (Infinito Positivo): {float.PositiveInfinity} || (Infinito Negativo) : {float.NegativeInfinity}");
-            Console.WriteLine($"Tipo float - (NaN): {float.NaN}");
-            Console.WriteLine($"Tipo double - (Minimo): {double.MinValue} || (Máximo) : {double.MaxValue}");
-            Console.WriteLine($"Tipo double - (Infinito Positivo): {double.PositiveInfinity} || (Infinito Negativo) : {double.NegativeInfinity}");
-            Console.WriteLine($"Tipo double - (NaN): {double.NaN}");
-            Console.WriteLine($"Tipo decimal - (Minimo): {decimal.MinValue} || (Máximo) : {decimal.MaxValue}");
+            uint i = uint.MaxValue;
+            Console.WriteLine(i + 1);
+            
             
         }
     }
