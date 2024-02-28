866da48d8d7ee80956682bb1ff30013256c27ac6
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 17:05:57 +0000

    Completed Exercise 5

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,19 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Introduz a altura do cilindro: ");
+            float altura_user = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.Write("Introduz o raio do cilindro: ");
+            float raio_user = Convert.ToSingle(Console.ReadLine());
+
+            float volume = MathF.PI * MathF.Pow(raio_user, 2) * altura_user;
+            float Area_Superficie = 2 * MathF.PI * raio_user * (raio_user + altura_user);
+
+            Console.WriteLine($"O Volume é {volume} e a Area de superficie é {Area_Superficie}!");
+
         }
     }
 }
