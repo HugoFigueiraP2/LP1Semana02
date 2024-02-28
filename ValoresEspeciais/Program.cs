5e20bb0f8bdb3cea3c1adbb39308cadf770da729
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-28 17:44:37 +0000

    Adding 2 types of overflows

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -8,6 +8,12 @@ namespace ValoresEspeciais
         {
             uint i = uint.MaxValue;
             Console.WriteLine(i + 1);
+
+            float x = 2 * float.MaxValue;
+            float y = float.MaxValue + 1;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
             
             
         }
