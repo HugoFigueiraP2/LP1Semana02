4818126ca3e2673fc868f3ebe7dfa623b07658cc
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-29 09:28:13 +0000

    Adding last questions and responses

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -20,7 +20,25 @@ namespace ChatGPTOne
                 else if (question == "How many constellations exists?")
                 {
                     Console.WriteLine("There are 88 constellations officially recognized by the IAU");
+                    continue;
                 }
+                else if (question == "Which is the temperature of the Moon?")
+                {
+                    Console.WriteLine("During the day, the temperature on the Moon can reach up to 127 degrees Celsius.");
+                    Console.WriteLine("During the night, it can drop to around -173 degrees Celsius!");
+                    continue;
+                }
+                else if (question == "Is it true that planet Mercury as ice?")
+                {
+                    Console.WriteLine("Yes, there is evidence of ice on Mercury, and this was confirmed by data from the MESSENGER mission in 2012.");
+                    continue;
+
+                }
+                else if(question == "EXIT")
+                {
+                    break;
+                }
+                else
 
                 
 
