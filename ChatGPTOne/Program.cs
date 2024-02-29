4c1c6f1446bc07bbe18f23e7c2581c813cf21bce
Hugo Silva <hugofigueira.dev@gmail.com>
2024-02-29 09:18:58 +0000

    Adding 2 questions and responses

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question;
+            while (true)
+            {
+                Console.Write("Place your question:");
+                question = Console.ReadLine();
+
+                if (question == "In which galaxy do we live?")
+                {
+                    Console.WriteLine("Via lactea");
+                    continue;
+                }
+                else if (question == "How many constellations exists?")
+                {
+                    Console.WriteLine("There are 88 constellations officially recognized by the IAU");
+                }
+
+                
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
