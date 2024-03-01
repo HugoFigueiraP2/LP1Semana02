c1cc4e165987fb588085b085c6874f2d3b126644
Hugo Silva <a22001815@alunos.ulht.pt>
2024-03-01 16:13:55 +0000

    Adding while and if statements

---
+++ MagicGun/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of shots: ")
+            int n = Convert.ToInt32(Console.ReadLine());
+            int turn = 1
+
+            while (turn<n + 1)
+            {
+                if (turn % 3 == 0)
+                {
+                    Console.Writeline()
+                }
+            }
+        }
+    }
+}
