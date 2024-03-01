872ec839678a88d86e18c59e2daf7409ea3a8694
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2024-03-01 16:40:27 +0000

    Completing Extra Exercice

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -6,16 +6,39 @@ namespace MagicGun
     {
         static void Main(string[] args)
         {
-            Console.Write("Number of shots: ")
+            Console.Write("Number of shots: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            int turn = 1
+            int turn = 0;
 
-            while (turn<n + 1)
-            {
-                if (turn % 3 == 0)
+            while (turn<n)
+            {   
+                ++turn;
+
+                if (turn % 5 == 0 && turn % 3 == 0)
                 {
-                    Console.Writeline()
+                    Console.WriteLine($"{turn}: Magic Fire! Eletric Fire!");
+                    continue;
                 }
+
+                else if (turn % 3 == 0)
+                {
+                    Console.WriteLine($"{turn}: Magic Fire! ");
+                    continue;
+                }
+
+                else if (turn % 5 == 0)
+                {
+                    Console.WriteLine($"{turn}: Eletric Fire! ");
+                    continue;
+                }
+
+                else
+                {
+                    Console.WriteLine($"{turn}: Normal fire :/");
+                    continue;
+                }
+
+
             }
         }
     }
